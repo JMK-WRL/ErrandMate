546c091a006633c8739c3e4722d5377afec97e0d
Jonathan Kyule <jonathankyule2@gmail.com>
2024-07-01 14:44:42 +0300

    Cleanup of some old files

--- errandmate/src/App.js
+++ errandmate/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignUpLogin from './components/SignUpLogin';
+import UserProfile from './components/UserProfile';
+import PostErrand from './components/PostErrand';
+import BrowseErrands from './components/BrowseErrands';
+import Chat from './components/Chat';
+import ReviewRating from './components/ReviewRating';
+import Payment from './components/Payment';
+import Dashboard from './components/Dashboard';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/signup-login" component={SignUpLogin} />
+        <Route path="/profile" component={UserProfile} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
