04ad0ec5bddf9f4c198257e4046fdde482c87fb3
ynwvroy <bennetfrmdao@gmail.com>
2024-07-01 14:22:36 +0300

    Implemented user sign up and login functionality

--- errandmate/src/components/SignUpLogin.js
+++ errandmate/src/components/SignUpLogin.js
@@ -1,2 +1,37 @@
 // src/components/SignUpLogin.js
+import React, { useState } from 'react';
+import { auth } from '../firebase';
+import { Button, TextField, Container } from '@material-ui/core';
+
+const SignUpLogin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = async () => {
+    try {
+      await auth.createUserWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleLogin = async () => {
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Container>
+      <TextField label="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <TextField label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <Button onClick={handleSignUp}>Sign Up</Button>
+      <Button onClick={handleLogin}>Login</Button>
+    </Container>
+  );
+};
+
+export default SignUpLogin;
 
