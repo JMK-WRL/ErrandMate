734fbda4c915d1a8e479a2227c6dc95db70b38e4
Jonathan Kyule <jonathankyule2@gmail.com>
2024-07-10 22:37:36 +0300

    Updated the Index files, where the app begins

--- errandmate/src/index.js
+++ errandmate/src/index.js
@@ -1,11 +1,16 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
-import './index.css'; // Create your own CSS if needed
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
